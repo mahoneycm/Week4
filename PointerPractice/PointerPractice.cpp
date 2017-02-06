5168b5a114653968399b297d8aacc87f23e3ec34
Owner1 <Owner1@OWNER>
2017-02-06 18:32:33 -0500

    Copy and paste practice

--- PointerPractice/PointerPractice.cpp
+++ PointerPractice/PointerPractice.cpp
@@ -1,11 +1,20 @@
 // PointerPractice.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 
 int main()
 {
-    return 0;
+	int i = 10;
+	int *ptr1 = &i;
+
+	cout << i << endl;
+	cout << ptr1 << endl;
+	cout << &i << endl;
+	cout << &ptr1 << endl;
+
+	return 0;
 }
 
